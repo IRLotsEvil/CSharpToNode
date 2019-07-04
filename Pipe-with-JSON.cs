83cdcbf07a9598db7ac40adc12c87b94af65d200
James Martinus <39163188+IRLotsEvil@users.noreply.github.com>
2019-07-04 12:01:53 +0100

    Update Pipe-with-JSON.cs

--- Pipe-with-JSON.cs
+++ Pipe-with-JSON.cs
@@ -6,10 +6,10 @@ public class ProcessPipe
     public ObservableCollection<string> Output { get; set; } = new ObservableCollection<string>();
     public ObservableCollection<string> Error { get; set; } = new ObservableCollection<string>();
     public bool RestartIfExited { get; set; } = true;
-    public ProcessPipe(string filename)
+    public ProcessPipe(string filename, string nodeAppDirectory)
     {
         Id = Guid.NewGuid().ToString();
-        var dir = Regex.Split(Directory.GetCurrentDirectory(), @"(?=.+)(\\ThroughPutSolution).+").Aggregate("", (a, c) => a + c) + @"\ThroughPutSolution\";
+        var dir = nodeAppDirectory;
         var file = Directory.GetFiles(dir, "electron.exe", SearchOption.AllDirectories).FirstOrDefault();
         if (file != null)
             MainProcess.StartInfo = new ProcessStartInfo(file)
